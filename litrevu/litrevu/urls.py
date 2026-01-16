221c9381abcf93958ffb0467b6c6fc943f55768b
ROM-XIX <romain.rozalen@gmail.com>
2026-01-16 15:40:15 +0100

    structure du projet + ajout d'une page de test d'affichage

--- litrevu/litrevu/urls.py
+++ litrevu/litrevu/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for merchex project.
+URL configuration for litrevu project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,11 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from listings import views
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("hello/", views.hello),
-    path("about-us/", views.about),  # ajoutez cette ligne
+    path("", include("authentification.urls")),
 ]
