518e333065a64336de09b5d0c36dd6f9c4b795a7
ROM-XIX <romain.rozalen@gmail.com>
2026-02-06 02:08:11 +0100

    ajout des models dans gestionlivre

--- litrevu/gestionlivre/models.py
+++ litrevu/gestionlivre/models.py
@@ -1,13 +1,40 @@
-# from django.conf import settings
-# from django.core.validators import MaxValueValidator, MinValueValidator
+# /P_09/litrevu/gestionlivre/models.py
+
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class Ticket(models.Model):
-    # Your Ticket model definition goes here
+    # Ticket model definition
     title = models.CharField(null=False, max_length=128)
-    auteur = models.CharField(null=True)
-    serial_number = models.IntegerField(unique=True, null=False)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class Review(models.Model):
+    # Review model definition
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class UserFollows(models.Model):
+    # UserFollows model definition
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following")
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followed_by"
+    )
+
+    class Meta:
+        # ensures we don't get multiple UserFollows instances
+        # for unique user-user_followed pairs
+        unique_together = ("user", "followed_user")
 
 
 # class Review(models.Model):
